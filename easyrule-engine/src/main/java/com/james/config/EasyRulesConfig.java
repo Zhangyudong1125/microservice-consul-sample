675c933f2bf3c17777dc20ead03d60fdcf21b79a
密力汤 <militang@militangdeMacBook-Pro.local>
2017-09-13 20:55:07 +0800

    add

---
+++ easyrule-engine/src/main/java/com/james/config/EasyRulesConfig.java
@@ -0,0 +1,37 @@
+/**
+ * Truderal.com.cn Inc.
+ * Copyright (c) 2016-2017 All Rights Reserved.
+ */
+package com.james.config;
+
+import org.easyrules.api.RulesEngine;
+import org.easyrules.core.RulesEngineBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.james.ruleengine.springrules.DummyRule;
+
+/**
+ * @author militang
+ * @version Id: EasyRulesConfi g.java, v 0.1 17/9/11 下午3:42 militang Exp $$
+ */
+@Configuration
+public class EasyRulesConfig {
+
+    @Bean
+    public DummyRule rule() {
+        return new DummyRule();
+    }
+
+    //    bean id="rule"class="samples.spring.DummyRule"scope="prototype"/>
+    //    bean id="rule"class="samples.spring.DummyRule"scope="prototype"/>
+
+    @Bean
+    public RulesEngine rulesEngine() {
+        RulesEngine rulesEngine = RulesEngineBuilder.aNewRulesEngine().build();//.getObject();//  rulesEngineFactoryBean
+        rulesEngine.registerRule(rule());
+        return  rulesEngine;
+    }
+
+
+}
